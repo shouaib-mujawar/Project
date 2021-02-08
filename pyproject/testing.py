f82126e22e1efe3e611075bc96d4d7ba43e98504
shouaib-mujawar <75732591+shouaib-mujawar@users.noreply.github.com>
2021-02-08 17:05:41 +0530

    Add files via upload

---
+++ pyproject/testing.py
@@ -0,0 +1,16 @@
+from Super import Super
+from User import User
+
+ob=Super('s1','john')
+ob.setquestion()
+ob.add_question()
+
+# ob1=User(1,'Sufiyan')
+# ob1.takeupquiz()
+# ob1.result()
+
+# ob2=User(2,"Shawn")
+# ob2.takeupquiz()
+# ob2.result()
+
+                
